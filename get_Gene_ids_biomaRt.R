20f73373f85d07fce7b93709b50e796039184c32
JLTrincado <juanluis.trincado@upf.edu>
2018-10-15 11:15:47 +0200

    Update get_Gene_ids_biomaRt.R

--- get_Gene_ids_biomaRt.R
+++ get_Gene_ids_biomaRt.R
@@ -2,10 +2,11 @@ args = commandArgs(trailingOnly=TRUE)
 
 #Get the gene name assoicated to the ensembl_gene_ids
 library(biomaRt)
+cat("Starting execution...")
 ensembl = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl")
 
 #Load the list of genes
-listAttributes(ensembl)
+# listAttributes(ensembl)
 # file <- read.table(file="/projects_rg/SCLC_cohorts/George/PSI_Junction_Clustering_v5/neoskipping_junctions_filtered_mut.tab",sep="\t",header = TRUE)
 file <- read.table(file=args[1],sep="\t",header = TRUE)
 genes <- unique(file$Gene_id)
@@ -17,3 +18,4 @@ file_merged <- merge(file,genes_associated_names,by.x="Gene_id",by.y="ensembl_ge
 #Save the new file
 # write.table(file_merged,file="/projects_rg/SCLC_cohorts/George/PSI_Junction_Clustering_v5/neoskipping_junctions_filtered_mut2.tab",quote=FALSE,row.names = FALSE,sep="\t")
 write.table(file_merged,file=args[2],quote=FALSE,row.names = FALSE,sep="\t")
+cat(paste0("Done.Created ",args[2],".\n"))
